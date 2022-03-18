2e7cfc6d833e683dd9289403bc2f6b77726a9b0d
David Cerniglia <dcerniglia@thedevshop.com>
2022-03-17 23:17:11 -0400

    feat(shortener): initial server setup

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`server started, listening on port ${PORT}`));
+

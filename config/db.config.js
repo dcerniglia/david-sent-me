0bafa82e8bc48e9de50df1200684c6e95cf02cef
David Cerniglia <dcerniglia@thedevshop.com>
2022-03-17 23:45:38 -0400

    wip feat(shortener): add post route for short url

--- config/db.config.js
+++ config/db.config.js
@@ -2,11 +2,17 @@ const mongoose = require('mongoose');
 
 const DB_URI = 'mongodb+srv://dcerniglia:e9HtDMMM%v^sp031*G@cluster0.ytij9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
-mongoose.connect(DB_URI, {
-	usenNewUrlParser: true,
-	useUnifiedTopology: true
-});
+const connectDB = async () => {
+	try {
+		mongoose.connect(DB_URI, {
+			usenNewUrlParser: true,
+			useUnifiedTopology: true
+		});
+	}
+	catch (err) {
+		console.error(err.message);
+		process.exit(1);
+	}
+}
 
-const connection = mongoose.connection;
-
-module.exports = connection;
+module.exports = connectDB;
---
+++ routes/urls.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router  = express.Router();
+const shortId = require('shortid');
+const url = require('../models/url')
+const validator = require('valid-url');
+
+router.post('/short', async (req, res) => {
+	req.bo
+})

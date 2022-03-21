57f6a27eccee25efbf1ac576c42b8cbd9c362f4f
David Cerniglia <dcerniglia@thedevshop.com>
2022-03-20 22:54:32 -0400

    feat(ui): add name component for title

---
+++ dsm-web/src/components/david.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function David() {
+	return (
+		<div>David</div>
+	)
+}
--- dsm-web/src/index.tsx
+++ dsm-web/src/index.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import David from './components/david';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <David />
   </React.StrictMode>,
   document.getElementById('root')
 );

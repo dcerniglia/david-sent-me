2cb230f1da15ce86e437c43dd32d047df739ca6c
David Cerniglia <dcerniglia@thedevshop.com>
2022-03-25 20:33:46 -0400

    wip working name loop

--- dsm-web-js/src/index.js
+++ dsm-web-js/src/index.js
@@ -1,6 +1,6 @@
 
 
-const button = document.querySelector('button');
+const button = document.querySelector('a');
 const container = document.querySelector('.container');
 
 button.addEventListener('click', () => {
@@ -8,4 +8,48 @@ button.addEventListener('click', () => {
 	p.innerText = 'David';
 	p.classList.add('name');
 	container.append(p);
+});
+
+
+
+
+
+const davids = [
+	'Beckham',
+	'Schwimmer',
+	'Matthews',
+	'Spade',
+	'Matthews',
+	'Attenborough',
+	'Hassselhoff',
+	'Blain',
+	'Letterman',
+	'Cross',
+	'Bowie',
+	'Tennant',
+	'Chapelle',
+	'Lynch',
+	'Franco',
+	'Gilmour',
+	'Carradine',
+	'Duchovny',
+	'Lee Roth',
+	'Caruso',
+	'Copperfield',
+	'Hyde Pierce',
+	'Byrn',
+	'Fincher',
+	'Navarro',
+	'Arquette',
+	'Mustaine',
+]
+
+davids.forEach((david, i) => {
+	setTimeout(() => {
+	const p = document.createElement('p');
+	p.innerText = david;
+	p.classList.add('name');
+	container.append(p);
+
+	}, i * 1000);
 });

cc7f0825ad9fe0833310437e761fc3bd8ec7aa47
David Cerniglia <dcerniglia@thedevshop.com>
2022-05-21 17:36:47 -0400

    everything is working

--- dsm-api/routes/index.ts
+++ dsm-api/routes/index.ts
@@ -3,7 +3,11 @@ import cors from 'cors';
 import { Url } from '../models/Url';
 
 const router: Router = express.Router();
-router.all('*', cors())
+const corsOptions = {
+  origin: 'http://127.0.0.1:5500',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+router.all('*', cors(corsOptions));
 
 router.get('/:code', async (req, res) => {
 	try {
